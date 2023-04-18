1f1330c6cdd2a9cb97c386c1af7b5357c24219a3
Baturpcu <batusalkim11@gmail.com>
2023-04-18 18:14:37 +0300

    update

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import HeroImg2 from '../components/HeroImg2';
 
 const Project = () => {
   return (
     <div>
       <Navbar/>
+      <HeroImg2 heading={"PROJECTS."} text={"Some of my most recetn works"}/>
       <Footer/>
     </div>
   )
